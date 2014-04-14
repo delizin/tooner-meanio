2c79f37275e894732320e9848d0bdf25ae8f9f55
Tom DeHart <tdehart@gmail.com>
2014-04-14 18:58:09 -0400

    forgot a comma

--- server/config/env/development.js
+++ server/config/env/development.js
@@ -3,7 +3,7 @@
 module.exports = {
     seedData: true,
     // db: 'mongodb://localhost/tooner-dev',
-    db: 'mongodb://tooner:123123@@oceanic.mongohq.com:10052/app24143670'
+    db: 'mongodb://tooner:123123@@oceanic.mongohq.com:10052/app24143670',
     app: {
         name: 'Tooner'
     }
