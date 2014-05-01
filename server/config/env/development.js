9065160c5f245d58c5d04b9d6c9ba3f788648257
Tom DeHart <tdehart@gmail.com>
2014-05-01 19:36:48 -0400

    heroku db

--- server/config/env/development.js
+++ server/config/env/development.js
@@ -2,8 +2,8 @@
 
 module.exports = {
     seedData: true,
-    db: 'mongodb://localhost/tooner-dev',
-    // db: 'mongodb://tooner:123123@oceanic.mongohq.com:10052/app24143670',
+    // db: 'mongodb://localhost/tooner-dev',
+    db: 'mongodb://tooner:123123@oceanic.mongohq.com:10052/app24143670',
     app: {
         name: 'Tooner'
     }
